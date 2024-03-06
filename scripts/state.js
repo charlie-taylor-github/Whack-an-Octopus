655c3373aa531b8cb131b1184d07b143118a2528
charlie-taylor-github <charliedbtaylor@gmail.com>
2024-03-06 14:28:02 +0000

    add border

--- scripts/state.js
+++ scripts/state.js
@@ -16,6 +16,12 @@ class State {
   }
 
   setPage(page) {
+    const validPages = [
+      'home-page', 'game-page', 'game-over-page', 'winner-page'
+    ];
+    if (!validPages.includes(page)) {
+      throw new Error(`Invalid page: ${page}`);
+    }
     this.#update({ page });;
   }
 }
