7c3724a90f90b923c5cb9596dba5118260191f94
charlie-taylor-github <charliedbtaylor@gmail.com>
2024-03-11 22:08:51 +0000

    document config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,17 +1,18 @@
 config = {
-  // GRID SETUP
-  GRID_SIZE: 3,
-  EMPTY_1: 4,
-  EMPTY_2: 4,
-  TARGET: 1,
-  BLUE_LIGHT: "c6eafb",
-  BLUE_DARK: "00aeef",
+  // GRID SETUP - EMPTY_1 + EMPTY_2 + TARGET must equal GRID_SIZE**2 (eg: 4 + 4 + 1 = 3**2 = 9)
+  GRID_SIZE: 3, // the width and height of the grid (eg 3 -> 3x3 = 9 cells total)
+  EMPTY_1: 4, // the number of cells with the first empty state
+  EMPTY_2: 4, // the number of cells with the second empty state
+  TARGET: 1, // the number of target cells
+  BLUE_LIGHT: "c6eafb", // colours for the EMPTY_1 cells
+  BLUE_DARK: "00aeef", // colours for the EMPTY_2 cells
 
   // GAME LOGIC
-  POINTS_PER_ROUND: 100,
-  WINNING_POINTS: 300,
-  INITIAL_TIME_PER_ROUND: 3,
-  GET_NEXT_TIME_PER_ROUND: function (timeForLastRound) {
+  POINTS_PER_ROUND: 1, // number of points awarded for hitting the target
+  WINNING_POINTS: 10, // the number of points needed to win the game
+  INITIAL_TIME_PER_ROUND: 3, // the amount of time given to hit the target in seconds
+
+  GET_NEXT_TIME_PER_ROUND: function (timeForLastRound) { // a function to calculate the time for the next round
     const timeForNextRound = timeForLastRound * 0.95;
     return timeForNextRound;
   },
